3c89faf15124088a2a88c44216da07c9244bf896
ViktorKonin <konin.vial@gmail.com>
2022-06-26 00:25:00 +0300

    HomeWork

---
+++ Qa34_ilcarro/src/test/java/tests/loginTests.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class loginTests extends TestBase {
+
+
+    //success login
+    @Test
+    public void successLogin() {
+        //3.open form
+        openLoginForm();
+
+        //4.fill form + valid data
+        fillLoginForm("viktor@gmail.com", "Vviktor12345$");
+
+        //5.submit login
+        submitLogin();
+
+        //6.Assert (is login success?) logout present?
+
+    }
+
+
+    // login negative
+    @Test
+    public void loginNegativeTestsWrongEmail() {
+
+        //3.open form
+        openLoginForm();
+
+        //4.fill form + invalid data
+        fillLoginForm("viktorgmail.com", "Vviktor12345$");
+
+        //5.submit login
+        submitLogin();
+        
+        //6.Assert (is login success?) logout present? NOT
+
+    }
+
+}
